5d41c486f41970db7f72c450444de2b03684acb5
Duane M.  Arzu <2020152202@ub.edu.bz>
2024-11-28 09:59:49 -0600

    10th commit

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -15,8 +15,7 @@ func (a *applicationDependencies) routes() http.Handler {
 
 	router.MethodNotAllowed = http.HandlerFunc(a.methodNotAllowedResponse)
 
-	// Books Section
-	// =============
+	// Section for Books
 	router.HandlerFunc(http.MethodGet, "/api/v1/healthcheck", a.requireActivatedUser(a.healthcheckHandler))
 	router.HandlerFunc(http.MethodGet, "/api/v1/books/:bid", a.requireActivatedUser(a.displayBookHandler))
 	router.HandlerFunc(http.MethodGet, "/api/v1/books", a.requireActivatedUser(a.listBooksHandler))
@@ -25,8 +24,7 @@ func (a *applicationDependencies) routes() http.Handler {
 	router.HandlerFunc(http.MethodPatch, "/api/v1/books/:bid", a.requireActivatedUser(a.updateBookHandler))
 	router.HandlerFunc(http.MethodDelete, "/api/v1/books/:bid", a.requireActivatedUser(a.deleteBookHandler))
 
-	// Reading Lists Section
-	// =====================
+	// Section for Reading Lists
 	router.HandlerFunc(http.MethodGet, "/api/v1/lists", a.requireActivatedUser(a.ReadinglistHandler))
 	router.HandlerFunc(http.MethodGet, "/api/v1/lists/:lid", a.requireActivatedUser(a.displayReadingListHandler))
 	router.HandlerFunc(http.MethodPost, "/api/v1/lists", a.requireActivatedUser(a.createReadingListHandler))
@@ -35,8 +33,7 @@ func (a *applicationDependencies) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/api/v1/lists/:lid/books", a.requireActivatedUser(a.addReadingListBookHandler))
 	router.HandlerFunc(http.MethodDelete, "/api/v1/lists/:lid/books", a.requireActivatedUser(a.RemoveReadingListBookHandler))
 
-	// Review Section
-	// ==============
+	// Section for Reviews
 	router.HandlerFunc(http.MethodPost, "/api/v1/books/:bid/reviews", a.requireActivatedUser(a.createReviewHandler))
 	router.HandlerFunc(http.MethodGet, "/api/v1/books/:bid/reviews", a.requireActivatedUser(a.bookReviewsHandler))
 	router.HandlerFunc(http.MethodGet, "/api/v1/books/:bid/reviews/:rid", a.requireActivatedUser(a.displayReviewHandler))
